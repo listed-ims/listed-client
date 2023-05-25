1083dbe96e3b3b23a7fadd7a00e57fe143037ee7
jlambojo <lambojojezelle@gmail.com>
2023-05-25 10:03:14 +0800

    Update Login.tsx

--- src/screens/home/Login.tsx
+++ src/screens/home/Login.tsx
@@ -49,8 +49,8 @@ const Login = ({ navigation }: LoginProps) => {
         </Column>
       </Box>
       <Box width="full">
-        <Button onPress={() => { navigation.navigate("Home") }}> Login </Button>
-        <Center>
+          <Button onPress={() => { handleLogin() }}> Login </Button>
+          <Center>
           <Row justifyContent="center" alignItems="center" marginTop="4">
             <Text>Don't have an account? </Text>
             <Link onPress={() => { navigation.navigate("Registration") }} _text={{
@@ -59,8 +59,8 @@ const Login = ({ navigation }: LoginProps) => {
               fontWeight: "medium" 
             }}>Sign Up. </Link> 
           </Row>
-        </Center>
-      </Box>
+          </Center>
+        </Box>
     </Flex>
     </ScreenContainer>
   )
