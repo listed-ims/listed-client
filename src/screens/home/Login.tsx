c63b87b481d2611f5e14bc6278ad38000b8ed884
jlambojo <lambojojezelle@gmail.com>
2023-05-26 00:26:41 +0800

    add form validation

--- src/screens/home/Login.tsx
+++ src/screens/home/Login.tsx
@@ -9,29 +9,58 @@ import { Ionicons } from "@expo/vector-icons";
 import { AuthContext } from '../../utils/context/AuthProvider'
 import { LoginCredentials } from '../../types/Logins'
 import { loginService } from '../../services/UserService'
-
 interface LoginProps {
   navigation: NativeStackNavigationProp<any>,
 }
 
 const Login = ({ navigation }: LoginProps) => {
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [errors, setErrors] = React.useState({
+    username: "",
+    password: "",
+  });
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
 
   const [show, setShow] = React.useState(false);
 
   const { loginAuth } = useContext(AuthContext);
 
+
+  const validate = () => {
+    if (formData.username === ""){
+      handleErrors("Please enter username.", "username");
+      return false;
+    } else if(formData.password === ""){
+      handleErrors("Please enter password.", "password")
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const handleErrors = (error: string, data: string) => {
+    setErrors({ ...errors, [data]: error });
+  };
+
+  const handleOnchange = (value: string, data: string) => {
+    setFormData({ ...formData, [data]: value });
+  };
+
   const handleLogin = () => {
-    loginService({ username: username, password: password } as LoginCredentials)
-      .then((response) => {
-        const token = response.data.token;
-        loginAuth(token);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+    if (validate()) {
+      loginService({ username: formData.username, password: formData.password } as LoginCredentials)
+        .then((response) => {
+          const token = response.data.token;
+          loginAuth(token);
+          console.log("Login success.");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   }
 
   return (
@@ -45,11 +74,25 @@ const Login = ({ navigation }: LoginProps) => {
         </Center>
         <Box flexGrow={1}>
           <Column space="4">
-            <FormControl label="Username">
-              <TextField value={username} onChangeText={setUsername} placeholder="Enter username..." />
+          <FormControl
+            label="Username"
+            errorMessage={errors.username}
+            isInvalid={errors.username !== ""}
+            >
+              <TextField
+                onFocus={() => handleErrors("", "username")} 
+                onChangeText={(value) => handleOnchange(value, "username")}
+                placeholder="Enter username..." />
             </FormControl>
-            <FormControl label="Password">
-              <TextField value={password} onChangeText={setPassword} placeholder="Enter password..."
+            <FormControl 
+              label="Password"
+              errorMessage={errors.password}
+              isInvalid={errors.password !== ""}
+            >
+              <TextField 
+                onFocus={() => handleErrors("", "password")}
+                onChangeText={(value) => handleOnchange(value, "password")}
+                placeholder="Enter password..."
                 type={show ? "text" : "password"}
                 rightElement={<Pressable onPress={() => setShow(!show)}>
                   <Icon as={<Ionicons name={show ? "eye" : "eye-off"} />} size={5} mr="2" color="muted.500" />
