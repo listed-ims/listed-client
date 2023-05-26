6a2a6313faf74d28c7fb659759fae2336df4b069
jlambojo <lambojojezelle@gmail.com>
2023-05-26 13:45:27 +0800

    added getUserService and modified home

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ScreenContainer from '../../layout/ScreenContainer'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
-import { Box, Column, Heading, Row, ScrollView, Text, VStack, useTheme } from 'native-base'
+import { Box, Column, Heading, Row, ScrollView, Text, VStack } from 'native-base'
 import MainButtons from '../../components/MainButtons'
 import SummaryCard from '../../components/SummaryCard'
 import TransactionActions from '../../components/TransactionButtons'
+import { getUserService } from '../../services/UserService'
+import { getToken } from '../../services/TokenStorage'
 
 
 interface HomeProps {
@@ -12,12 +14,32 @@ interface HomeProps {
 }
 
 const Home = ({ navigation }: HomeProps) => {
-  const theme = useTheme();
+  const [username, setUsername] = useState("");
+
+  useEffect(() => { 
+    const fetchData = async () => {
+      const token = await getToken();
+      if (token) {
+        getUserService(token)
+          .then((response) => {
+            const username = response.data.username;
+            setUsername(username);
+            console.log(username);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }
+    }
+    fetchData();
+   },[])
+
+  
   return (
     <ScreenContainer>
-    <Column space="4" height="full" paddingTop="4">
+    <Column space="4" height="full" paddingTop="5">
       <ScrollView>
-        <Text fontWeight="bold" color="muted.400" fontSize="lg">Hi, John Doe</Text>
+        <Text fontWeight="bold" color="muted.400" fontSize="lg">Hi, {username}.</Text>
         <Text fontWeight="bold" fontSize="2xl">Welcome!</Text>
         <VStack space="7" overflowX="auto">
           <Box paddingTop="6">
