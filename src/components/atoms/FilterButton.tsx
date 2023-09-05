fba22d6f3ccb86b73ce0970a70a4596aff74d687
Faith Rosalijos <faithrosalijos@gmail.com>
2023-09-05 18:32:23 +0800

    created filter button

---
+++ src/components/atoms/FilterButton.tsx
@@ -0,0 +1,28 @@
+
+import { Button } from 'native-base'
+import { IButtonProps } from 'native-base'
+import React from 'react'
+
+interface ChipProps extends IButtonProps {
+  label: string,
+  state: "active" | "inactive"
+}
+
+const FilterButton = ({ label, state, ...props }:ChipProps) => {
+  return (
+    <Button 
+      {...props}
+      borderRadius="full"
+      size="xs"
+      px="4"
+      background={state === "active" ? "primary.700" : "muted.100"}
+      _text={{
+        fontSize: "xs", fontWeight: "semibold",
+        color: `${state === "active" ? "lightText" : "darkText"}`
+      }}>
+      {label.toUpperCase()}
+    </Button>
+  )
+}
+
+export default FilterButton
