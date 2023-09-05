a0f1d65b57d7819222f7258dc834901b2436df10
Faith Rosalijos <faithrosalijos@gmail.com>
2023-09-05 21:38:10 +0800

    added on pressed state to filter buttons

--- src/components/atoms/FilterButton.tsx
+++ src/components/atoms/FilterButton.tsx
@@ -1,28 +1,32 @@
-
-import { Button } from 'native-base'
-import { IButtonProps } from 'native-base'
-import React from 'react'
+import { Button } from "native-base";
+import { IButtonProps } from "native-base";
+import React from "react";
 
 interface ChipProps extends IButtonProps {
-  label: string,
-  state: "active" | "inactive"
+  label: string;
+  state: "active" | "inactive";
 }
 
-const FilterButton = ({ label, state, ...props }:ChipProps) => {
+const FilterButton = ({ label, state, ...props }: ChipProps) => {
   return (
-    <Button 
+    <Button
       {...props}
       borderRadius="full"
       size="xs"
       px="4"
       background={state === "active" ? "primary.700" : "muted.100"}
+      _pressed={{
+        backgroundColor: `${state === "active" ? "primary.600" : "muted.200"}`,
+      }}
       _text={{
-        fontSize: "xs", fontWeight: "semibold",
-        color: `${state === "active" ? "lightText" : "darkText"}`
-      }}>
+        fontSize: "xs",
+        fontWeight: "semibold",
+        color: `${state === "active" ? "lightText" : "darkText"}`,
+      }}
+    >
       {label.toUpperCase()}
     </Button>
-  )
-}
+  );
+};
 
-export default FilterButton
+export default FilterButton;
