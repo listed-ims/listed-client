8bc3ee8746f05fd5ea69ac7000f0eba5d79f59ff
jlambojo <lambojojezelle@gmail.com>
2023-09-12 16:00:51 +0800

    added modal component

---
+++ src/components/organisms/InvalidLoginModal.tsx
@@ -0,0 +1,40 @@
+import { Button } from '@listed-components/atoms'
+import { ModalContent } from '@listed-types';
+import { Text, Modal, Row } from 'native-base'
+import { InterfaceModalProps } from 'native-base/lib/typescript/components/composites/Modal/types';
+
+interface InvalidLoginModalProps extends InterfaceModalProps {
+  modalContent: ModalContent
+  onClose: () => void;
+}
+
+const InvalidLoginModal: React.FC<InvalidLoginModalProps> = ({ modalContent, onClose, ...props }) => {
+    
+  return (
+    <Modal {...props} paddingX="4">
+      <Modal.Content width="full">
+          <Modal.Header alignItems="center">
+            {modalContent.header}
+          </Modal.Header>
+          <Modal.Body>
+            <Text textAlign="center">
+              {modalContent.body}
+            </Text>
+          </Modal.Body>
+          <Modal.Footer>
+          <Row 
+          width="full"
+          justifyContent="center">
+            <Button
+            flex="1"
+            variant="subtle" onPress={onClose}>
+              OK
+            </Button>
+          </Row>
+          </Modal.Footer>
+        </Modal.Content>
+    </Modal>
+  )
+}
+
+export default InvalidLoginModal
--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,2 +1,3 @@
 export { default as ScreenContainer } from "./ScreenContainer";
 export { default as ProductList } from "./ProductList";
+export { default as InvalidLoginModal } from "./InvalidLoginModal";
