8a5a4d5be3f2e8642d43212814c3fad833c56d2e
Jemwel Odo <odojemwel@gmail.com>
2023-10-29 21:26:24 +0800

    removed select component import

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -4,7 +4,6 @@ export { default as TransactionButton } from "./TransactionButton";
 export { default as FormControl } from "./FormControl";
 export { default as TextField } from "./TextField";
 export { default as BarcodeField } from "./BarcodeField";
-export { default as Select } from "./Select";
 export { default as ProductListItem } from "./ProductListItem";
 export { default as StoreListItem } from "./StoreListItem";
 export { default as StoreListFilterGroup } from "./StoreListFilterGroup";
