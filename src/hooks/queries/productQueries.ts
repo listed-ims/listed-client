eb19ac8097a87540572dde2abc144e954aee23b1
Dave Roy L. Capalaran <126816093+dr-cplrn@users.noreply.github.com>
2023-10-29 23:27:55 +0800

    fix incoming sale price validation

--- src/hooks/queries/productQueries.ts
+++ src/hooks/queries/productQueries.ts
@@ -58,6 +58,7 @@ export const useGetProductDetails = (productId: number) => {
     () => getProductService(productId),
     {
       staleTime: fiveMinutes,
+      enabled: !!productId,
     }
   );
 };
